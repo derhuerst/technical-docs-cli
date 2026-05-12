8643a9e6f19bb91d441a6860a1bfb76a569767ba
Jannis R <mail@jannisr.de>
2026-05-13 00:07:26 +0200

    refactor tests ✅

--- test.js
+++ test.js
@@ -11,6 +11,24 @@ const failWithError = (err) => {
 	process.exit(1)
 }
 
+const render = (rendererCfg, input) => {
+	return new Promise((resolve, reject) => {
+		const renderer = createMarkdownRenderer(rendererCfg)
+		setImmediate(() => {
+			renderer.end(input)
+		})
+
+		renderer.on('error', reject)
+		let res = ''
+		renderer.on('data', (chunk) => {
+			res += chunk
+		})
+		renderer.on('end', () => {
+			resolve(res)
+		})
+	})
+}
+
 const t1 = `\
 - [internal relative](/foo/readme.md)
 - [external absolute](https://github.com/derhuerst/technical-docs-cli/blob/foo/bar/readme.md)
@@ -33,31 +51,27 @@ pipeline.process(t1, (err, file) => {
 	ok(file.contents.includes(
 		'https://github.com/derhuerst/technical-docs-cli/blob/foo/bar/readme.md'
 	), 'output does not contain external absolute link')
+
+	console.log('pipeline.process() seems to work ✔️')
 })
 
 {
-	const renderer = createMarkdownRenderer({
-		additionalHeadChildren: (h) => [
-			h('meta', {property: 'foo', content: 'bar'}),
-		],
-	})
-	renderer.end(t1)
-
-	renderer.once('error', failWithError)
-	let res = ''
-	renderer.on('data', (chunk) => {
-		res += chunk
-	})
-	renderer.once('end', () => {
-		console.error('res', res) // todo: remove
+	render({
+		syntaxStylesheetUrl: '/foo.csv',
+	}, t1)
+	.then((res) => {
 		ok(res.includes(
 			'<link rel="stylesheet" href="/foo.csv"',
 		), 'output does not include custom link stylesheet link')
+
+		console.log('renderer Stream seems to work ✔️')
 	})
+	.catch(failWithError)
 }
 
 {
 	const githubCssPath = determineSyntaxStylesheetPath('github')
 	strictEqual(typeof githubCssPath, 'string', 'determineSyntaxStylesheetPath() must return a string')
 	readFileSync(githubCssPath)
+	console.log('syntax stylesheet file exists ✔️')
 }
